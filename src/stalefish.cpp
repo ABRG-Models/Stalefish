b51183171556056da08dee52d93273da11392fa8
Seb James <seb.james@sheffield.ac.uk>
2020-01-21 14:13:27 +0000

    Data manager singleton now works

--- src/stalefish.cpp
+++ src/stalefish.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <opencv2/opencv.hpp>
 #include <opencv2/imgproc.hpp>
 #include <iostream>
@@ -9,133 +10,122 @@
 using namespace cv;
 using namespace std;
 
-//! Singleton pattern class to hold framedata
-class DataManager
+//! Singleton pattern data manager class to hold framedata
+class DM
 {
 private:
     //! Private constructor/destructor
-    DataManager();
-    ~DataManager();
-
+    DM() {};
+    ~DM() {};
     //! A pointer returned to the single instance of this class
-    static DataManager* pInstance;
-
+    static DM* pInstance;
     //! The frame data which is being managed
-    vector<FrameData> frames;
-
+    vector<FrameData> vFrameData;
     //! The index into frames which is the current image
     int I = 0;
-
     //! The current image
     Mat img;
 
 public:
-    static DataManager* instance (void) {
-        if (DataManager::pInstance == 0) {
-            DataManager::pInstance = new DataManager;
+    //! The instance public function. Short on purpose
+    static DM* i (void) {
+        if (DM::pInstance == 0) {
+            DM::pInstance = new DM;
         }
-        return DataManager::pInstance;
+        return DM::pInstance;
     }
-
-    void addFrame (FrameData& fr) {
-        this->frames.push_back (fr);
+    //! Add a frame to vFrameData
+    void addFrame (Mat& frameImg) {
+        this->vFrameData.push_back (FrameData (frameImg.clone()));
     }
-
-    vector<FrameData>* getFrames (void) {
-        return &(this->frames);
-    }
-
+    //! Return the size of vFrameData
     unsigned int getNumFrames (void) {
-        return this->frames.size();
+        return this->vFrameData.size();
     }
-
-    FrameData* getCurrentFrame (void) {
-        if (!this->frames.empty()) {
-            return &(this->frames[I]);
+    //! get current frame. Short name on purpose.
+    FrameData* gcf (void) {
+        if (!this->vFrameData.empty()) {
+            return &(this->vFrameData[I]);
         }
         return (FrameData*)0;
     }
-
-    int getI (void) {
-        return I;
+    //! Get the current frame number, counting from 1 like a human.
+    int getFrameNum (void) {
+        return 1+I;
     }
-
+    // Get a pointer to the persistent Mat img member attribute
     Mat* getImg (void) {
         return &(img);
     }
-
+    //! Make the next frame current (or cycle back to the first)
     void nextFrame (void) {
-        ++I;
-        if (I == this->frames.size()) {
-            // Ooops, off the end, restep
-            --I;
-        }
+        ++I %= this->vFrameData.size();
     }
-
-    void previousFrame (void) {
-        --I;
-        if (I < 0) {
-            // Ooops, off the end, restep
-            ++I;
-        }
-    }
-
     //! Clone the current frame into Mat img
     void cloneFrame (void) {
-        this->img = this->frames[I].frame.clone();
+        this->img = this->vFrameData[I].frame.clone();
     }
 };
 
 // Globally init datamanager instance pointer to null
-DataManager* DataManager::pInstance = 0;
+DM* DM::pInstance = 0;
 
 // *** DISPLAY ***
-void onmouse(int event, int x, int y, int flags, void* param) {
-    cout << "onmouse called" << endl;
-    Point pt = Point(x,y);
-    if (event==CV_EVENT_LBUTTONDOWN) {
-        DataManager::instance()->getCurrentFrame()->P.push_back(pt);
-    }
-    DataManager::instance()->cloneFrame();
+void onmouse (int event, int x, int y, int flags, void* param) {
 
-    circle(img,pt,5,Scalar(0,0,255),1);
-    for(int i=1;i<DataManager::instance()->getCurrentFrame()->P.size();i++){
-        line(img,DataManager::instance()->getCurrentFrame()->P[i-1],DataManager::instance()->getCurrentFrame()->P[i],Scalar(255,0,0),1);
+    Point pt = Point(x,y);
+    if (event == CV_EVENT_LBUTTONDOWN) {
+        DM::i()->gcf()->P.push_back(pt);
     }
-    for(int i=0;i<DataManager::instance()->getCurrentFrame()->P.size();i++){
-        circle(img,DataManager::instance()->getCurrentFrame()->P[i],5,Scalar(255,0,0),1);
+    DM::i()->cloneFrame();
+
+    // Make copies of pointers to neaten up the code, below
+    Mat* pImg = DM::i()->getImg();
+    FrameData* cf = DM::i()->gcf();
+
+    circle (*pImg, pt, 5, Scalar(0,0,255), 1);
+
+    for (int i=1; i<cf->P.size(); i++) {
+        line (*pImg, cf->P[i-1], cf->P[i], Scalar(255,0,0), 1);
     }
-    cout << "fors mid" << endl;
-    if (DataManager::instance()->getCurrentFrame()->P.size()){
-        line(img,DataManager::instance()->getCurrentFrame()->P[DataManager::instance()->getCurrentFrame()->P.size()-1],pt,Scalar(255,0,0),1);
+    for (int i=0; i<cf->P.size(); i++){
+        circle (*pImg, cf->P[i], 5, Scalar(255,0,0), 1);
     }
-    cout << "fors mid2" << endl;
-    for(int i=1;i<DataManager::instance()->getCurrentFrame()->fitted.size();i++){
-        line(img,DataManager::instance()->getCurrentFrame()->fitted[i-1],DataManager::instance()->getCurrentFrame()->fitted[i],Scalar(0,255,0),1);
+
+    if (cf->P.size()) {
+        line (*pImg, cf->P[cf->P.size()-1], pt, Scalar(255,0,0),1);
     }
-    cout << "fors mid3" << endl;
-    line(img,DataManager::instance()->getCurrentFrame()->axis[0],DataManager::instance()->getCurrentFrame()->axis[1],Scalar(0,0,255),1);
-    cout << "fors mid4" << endl;
-    for(int i=0;i<DataManager::instance()->getCurrentFrame()->origins.size();i++){
-        line(img,DataManager::instance()->getCurrentFrame()->origins[i],DataManager::instance()->getCurrentFrame()->tangents[i],Scalar(0,255,255),1);
+
+    for(int i=1; i<cf->fitted.size(); i++) {
+        line (*pImg, cf->fitted[i-1], cf->fitted[i], Scalar(0,255,0), 1);
     }
-    cout << "fors done" << endl;
+
+    line (*pImg, cf->axis[0], cf->axis[1], Scalar(0,0,255), 1);
+
+    for (int i=0; i<cf->origins.size(); i++) {
+        line (*pImg, cf->origins[i], cf->tangents[i], Scalar(0,255,255), 1);
+    }
+
     stringstream ss;
-    ss<<"Frame: "<<DataManager::instance()->getI()<<"/"<<DataManager::instance()->getNumFrames()<<", "<<"Poly order: "<<DataManager::instance()->getCurrentFrame()->polyOrder<<", Bins: "<<DataManager::instance()->getCurrentFrame()->nBins;
-    putText(img,ss.str(),Point(30,30), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,0), 1, CV_AA);
-    cout << "imshow()..." << endl;
-    imshow("StaleFish", img);
+    ss << "Frame: " << DM::i()->getFrameNum() << "/" << DM::i()->getNumFrames()
+       << ", Poly order: " << cf->polyOrder
+       << ", Bins: " << cf->nBins;
+    putText (*pImg, ss.str(), Point(30,30), FONT_HERSHEY_SIMPLEX, 0.8, Scalar(0,0,0), 1, CV_AA);
+
+    imshow ("StaleFish", *pImg);
 }
 
 // *** MAIN PROGRAM ***
 
 int main(int argc, char** argv){
 
-    cout << "Start" << endl;
-    F.clear();
+    if (argc < 2) {
+        cout << "Please supply at least one image filename" << endl;
+        return 1;
+    }
+
     double lenA = 10.;
     double lenB = 50.;
-    I = 0;
     for (int i=1;i<argc;i++){
         char* imageName = argv[i];
         cout << "imread " << imageName << endl;
@@ -144,20 +134,14 @@ int main(int argc, char** argv){
             cout <<  "Could not open or find the image" << endl;
             return -1;
         }
-        cout << "push_back on F" << endl;
-        F.push_back (FrameData (frame.clone()));
+        DM::i()->addFrame (frame);
     }
 
     // Fails to draw the window
-    namedWindow("StaleFish", 1);
-    //img = F[0].frame.clone();
-    //imshow("Stalefish", img);
-
-    // Causes "no opengl in opencv" error
-    //updateWindow("StaleFish");
+    namedWindow("StaleFish", WINDOW_AUTOSIZE);
 
     cout << "Set mouse callback..." << endl;
-    setMouseCallback("StaleFish", onmouse, &img);
+    setMouseCallback("StaleFish", onmouse, DM::i()->getImg());
     cout << "Done setting mouse callback" << endl;
 
     // *** MAIN LOOP ***
@@ -165,24 +149,49 @@ int main(int argc, char** argv){
     while (1) {
         onmouse(CV_EVENT_MOUSEMOVE,0,0,0,NULL);
         char k = waitKey(0);
-        switch(k){
-            case('x'):{
-                DataManager::instance()->getCurrentFrame()->updateFit();
-                DataManager::instance()->getCurrentFrame()->refreshBoxes(lenA,lenB);
-                DataManager::instance()->getCurrentFrame()->getBoxMeans();
-            } break;
-            case('c'):{ DataManager::instance()->getCurrentFrame()->removeLastPoint(); } break;
-            case ('n'):{ I++; I%=F.size();} break;
-            case ('p'):{
-                DataManager::instance()->getCurrentFrame()->polyOrder ++;
-                DataManager::instance()->getCurrentFrame()->polyOrder %= 10;
-            } break;
-            case ('b'):{
-                DataManager::instance()->getCurrentFrame()->nBins ++;
-                DataManager::instance()->getCurrentFrame()->nBins %= 100;
-            } break;
-            case ('w'):{ DataManager::instance()->getCurrentFrame()->printMeans(); } break;
-            case ('q'):{return 0;} break;
+        switch(k) {
+        case('x'):
+        {
+            DM::i()->gcf()->updateFit();
+            DM::i()->gcf()->refreshBoxes(lenA,lenB);
+            DM::i()->gcf()->getBoxMeans();
+            break;
         }
+        case('c'):
+        {
+            DM::i()->gcf()->removeLastPoint();
+            break;
+        }
+        case ('n'):
+        {
+            DM::i()->nextFrame();
+            break;
+        }
+        case ('p'):
+        {
+            DM::i()->gcf()->polyOrder ++;
+            DM::i()->gcf()->polyOrder %= 10;
+            break;
+        }
+        case ('b'):{
+            DM::i()->gcf()->nBins ++;
+            DM::i()->gcf()->nBins %= 100;
+            break;
+        }
+        case ('w'):
+        {
+            DM::i()->gcf()->printMeans();
+            break;
+        }
+        case ('q'):
+        {
+            return 0;
+            break;
+        }
+        default:
+            break;
+        }
+        // Allow graphing system to catch up
+        usleep (1000);
     }
 }
