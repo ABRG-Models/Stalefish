2dfeffdbeb3d49194402cfd9e62d0fafe7bc0ffd
Seb James <seb.james@sheffield.ac.uk>
2020-02-26 10:34:30 +0000

    minor changes. going to drop this for now

--- allen/plotcolors.m
+++ allen/plotcolors.m
@@ -42,15 +42,15 @@ clf
 hold on
 ex_col = [111,0,207]./255.0
 nex_col = [12,140,84]./255.0
-rx = [];
-ry = [];
+blue = [];
+redgreen = [];
 for ii = 1:len2
     ex_x = [c(ii,1)];%b
     ex_y = [c(ii,2)];%g
     ex_z = [c(ii,3)];%r
     plot3(ex_x,ex_y,ex_z,'o','markeredgecolor',ex_col,'markersize', 15, 'markerfacecolor', flip(c(ii,:)./255.0));
-    rx = [rx; ex_x];
-    ry = [ry; ex_y, ex_z];
+    blue = [blue; ex_x];
+    redgreen = [redgreen; ex_y, ex_z];
 end
 
 plot3([0 255], [0 0], [0 0], 'b-');
@@ -62,6 +62,10 @@ ylabel ('Green channel')
 zlabel ('Red channel')
 
 % Linear regression. Regress red/green against blue
-b = rx\ry;
-ry_calc = b .* rx;
-plot3 (rx, ry_calc(:,1), ry_calc(:,2), 'k-')
+b = blue\redgreen;
+redgreen_calc = b .* blue;
+plot3 (blue, redgreen_calc(:,1), redgreen_calc(:,2), 'k-')
+
+% Add the fit line to fig 1 too
+figure(1)
+plot3 (blue, redgreen_calc(:,1), redgreen_calc(:,2), 'k-')
