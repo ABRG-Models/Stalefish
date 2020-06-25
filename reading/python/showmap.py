834700ed24c5744a5586d76be90ec49570ed80c3
Seb James <seb.james@sheffield.ac.uk>
2020-06-25 15:48:14 +0100

    Script now shows both means and autoscaled means

--- reading/python/showmap.py
+++ reading/python/showmap.py
@@ -31,17 +31,21 @@ with h5py.File (filename, 'r') as f:
     x = []
     y = []
     means = []
+    means_autoscaled = []
     thicks = []
 
     for i in range(1,nf+1):
 
         # Create the 'frame tag' (e.g. 'Frame000')
         frame = 'Frame{0:03}'.format(i)
-        #print ('frame: {0}'.format(frame))
+        print ('frame: {0}'.format(frame))
 
         # The means of the sampled boxes are in means or means_autoscaled
-        key = '{0}/means_autoscaled'.format(frame)
+        key = '{0}/means'.format(frame)
         means_ = list(f[key])
+
+        key = '{0}/means_autoscaled'.format(frame)
+        means_autoscaled_ = list(f[key])
 
         # The position on the 3D x-axis is in class/layer_x:
         key = '{0}/class/layer_x'.format(frame)
@@ -53,6 +57,7 @@ with h5py.File (filename, 'r') as f:
         # For y values, use sbox_linear_distance
         key = '{0}/sbox_linear_distance'.format(frame)
         y_ = list(f[key])
+        #print ('y_ = {0}'.format(y_))
 
         # Use the per-frame slice thickness when plotting rectangular polygons
         key = '{0}/class/thickness'.format(frame)
@@ -63,12 +68,15 @@ with h5py.File (filename, 'r') as f:
         x.append(x_)
         y.append(y_)
         means.append(means_)
+        means_autoscaled.append(means_autoscaled_)
         thicks.append(thicks_)
 
     # We now have (as lists of lists) x, y, means and the thicknesses
     # (in dimension x) of all the boxes. A regular scatter graph gives
     # awful results. Instead, plot each x/y as a rectangle with colour
     # given by means.
+    F = plt.figure()
+    F.add_subplot (2,1,1)
     plt.title('Flattened heat plot')
     for ii in range(0,len(x)): # len(x) is the number of frames.
         # Within each frame, make polygons
@@ -84,11 +92,36 @@ with h5py.File (filename, 'r') as f:
                            [ x_r[jj],  y_b[jj] ],
                            [ x_r[jj], y_t[jj] ],
                            [ x_l[jj], y_t[jj] ] ])
-            poly = Polygon (v, facecolor=mpl.cm.jet(means[ii][jj]), edgecolor='None')
+            poly = Polygon (v, facecolor=mpl.cm.Greys(means[ii][jj]), edgecolor='None')
             plt.gca().add_patch(poly)
 
 
     plt.xlabel('x')
     plt.ylabel('y')
     plt.autoscale()
+
+    F.add_subplot (2,1,2)
+    plt.title('Flattened heat plot (autoscale)')
+    for ii in range(0,len(x)): # len(x) is the number of frames.
+        # Within each frame, make polygons
+        x_l = np.array(x[ii])
+        x_r = np.add (np.array(x[ii]), np.array(thicks[ii]))
+        y_ar = np.array(y[ii]) # convert the list y[ii] into a np array
+        y_t = y_ar + (shift(y_ar, -1, cval=y_ar[-1]) - y_ar) / 2.0 # half way between y and the next y
+        y_b = y_ar - (y_ar - shift(y_ar, 1, cval=y_ar[0])) / 2. # half way between y and the previous y
+
+        for jj in range(0,len(x[ii])): # len(x[ii]) is the number of bins in a frame
+            # Boxes have vertices: (x_l,y_b) -> (x_r,y_b) -> (x_r,y_t) -> (x_l,y_t)
+            v = np.array([ [ x_l[jj], y_b[jj] ], # v stands for 'vertices'
+                           [ x_r[jj],  y_b[jj] ],
+                           [ x_r[jj], y_t[jj] ],
+                           [ x_l[jj], y_t[jj] ] ])
+            poly = Polygon (v, facecolor=mpl.cm.Greys(means_autoscaled[ii][jj]), edgecolor='None')
+            plt.gca().add_patch(poly)
+
+
+    plt.xlabel('x')
+    plt.ylabel('y')
+    plt.autoscale()
+    plt.tight_layout()
     plt.show()
