ffbd82dc1def76c162117bdc469963e01861388b
Seb James <seb.james@sheffield.ac.uk>
2021-06-01 13:24:37 +0100

    Updated in line with the fact that only E18.5 data is used in the paper

--- allen/retrieve_forpaper.sh
+++ allen/retrieve_forpaper.sh
@@ -2,7 +2,7 @@
 
 # Retrieve Developing Mouse Data for Gene Id2
 
-# Dev time point E18.5
+# Dev time point E18.5 (used in Extended Data Figure 1)
 python3 retrieve.py 100076267
-# Dev time point P4
-python3 retrieve.py 100081391
+# Dev time point P4 (not used in paper)
+#python3 retrieve.py 100081391
