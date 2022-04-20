cec2db74dd4d1335609d4c9abd4fa34336079913
Seb James <seb.james@sheffield.ac.uk>
2022-04-20 09:40:58 +0100

    Tweak

--- allen/template_mouse_brain/threemaps.sh
+++ allen/template_mouse_brain/threemaps.sh
@@ -1,1 +1,1 @@
-./build/src/sfview -a -m1 602630314_annot.h5 P9_Mouse_2_Litter_4_RZRB.h5 ~/gdrive_inshare/Stalefish/SebJames/Vole_P1/Rzrb/46_12A_rzrb_L4.h5  -M=16,36 -T -x -0.5,12,-23,12
+./build/src/sfview -a -m1 atlas/602630314_annot/602630314_annot.h5 P9_Mouse_2_Litter_4_RZRB.h5 ~/gdrive_inshare/Stalefish/SebJames/Vole_P1/Rzrb/46_12A_rzrb_L4.h5  -M=16,36 -T -x -0.5,12,-23,12
